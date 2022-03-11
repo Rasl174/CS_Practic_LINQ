1728943269dc3e6d0d884e8ac96a1a33d4dc807d
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-03-11 12:11:17 +0500

    Добавьте файлы проекта.

---
+++ CS_Practic_LINQ/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Practic_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countNumbers = 0;
+            int threeDigitNumbers;
+
+            for (int N = 1; N <= 27; N++)
+            {
+                threeDigitNumbers = N * N;
+
+                if(threeDigitNumbers > 99 && threeDigitNumbers <= 999)
+                {
+                    countNumbers++;
+                }
+            }
+            Console.WriteLine(countNumbers);
+        }
+    }
+}
---
+++ CS_Practic_LINQ/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("CS_Practic_LINQ")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CS_Practic_LINQ")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("284285e4-a275-4c62-a18f-e8b2c76eb9f1")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
